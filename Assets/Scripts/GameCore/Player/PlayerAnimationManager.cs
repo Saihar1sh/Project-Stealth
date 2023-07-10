f90e211a0d350483818b604ccb758cb1c606b56a
Sai Harish <30901561+Saihar1sh@users.noreply.github.com>
2023-07-10 18:31:24 +0530

    Animations added

---
+++ Assets/Scripts/GameCore/Player/PlayerAnimationManager.cs
@@ -0,0 +1,34 @@
+using Gameplay.Inputs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationManager : MonoBehaviour
+{
+    public Animator playerAnimController;
+
+    const string normalVelocityX = "normal_velocityX", normalVelocityY = "normal_velocityY";
+
+    public int normalVelX = 0, normalVelY = 0;
+
+    private void Awake()
+    {
+        normalVelX = Animator.StringToHash(normalVelocityX);
+        normalVelY = Animator.StringToHash(normalVelocityY);
+
+    }
+
+    private void Start()
+    {
+    }
+
+    public void PlayerVelocityHandler(Vector3 velocity)
+    {
+        playerAnimController.SetFloat(normalVelX,velocity.x);
+        playerAnimController.SetFloat(normalVelY,velocity.y);
+    }
+
+    private void OnDestroy()
+    {
+    }
+}
