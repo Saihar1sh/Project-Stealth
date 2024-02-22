85455420a4eed0851ff7c4ea994cce4f3a21f350
Sai Harish <30901561+Saihar1sh@users.noreply.github.com>
2024-02-22 12:22:18 +0530

    Weapon aiming rig done

--- Assets/Scripts/GameCore/Player/PlayerAiming.cs
+++ Assets/Scripts/GameCore/Player/PlayerAiming.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
 
 namespace Gameplay.Player
 {
     public class PlayerAiming : MonoBehaviour
     {
         public float turnSpeed = 15f;
+        public float aimDuration = .3f;
 
+        [SerializeField]private Rig aimLayer;
+        
         private Camera _mainCamera;
 
         // Start is called before the first frame update
@@ -22,6 +27,18 @@ namespace Gameplay.Player
             float yawCamera = _mainCamera.transform.rotation.eulerAngles.y;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0),
                 turnSpeed * Time.fixedDeltaTime);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                aimLayer.weight += Time.deltaTime / aimDuration;
+            }
+            else
+            {
+                aimLayer.weight -= Time.deltaTime / aimDuration;
+            }
         }
     }
 }
